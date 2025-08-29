5f41d5cbc4ce7944bdafaba599dd214216c71c7c
Rino Godson <rinogodson@gmail.com>
2025-08-29 11:04:23 +0530

    gravity inversion working!

--- main.gd
+++ main.gd
@@ -3,23 +3,39 @@ extends Node2D
 @onready var camera = $Camera2D
 @onready var kitty = $kitty
 @onready var tilemap = $TileMapLayer
+@onready var info = $Info
 
 var screen_size : Vector2i
 	
 var chunk_width : float
 var lev : bool
 
+@export var gamerunning : bool
+
 func _ready() -> void:
+	gamerunning = false
+	$kitty.gamerunning = gamerunning
 	screen_size = get_window().size
 	var used_rect = tilemap.get_used_rect()
 	var cell_size = tilemap.tile_set.tile_size
 	chunk_width = float(used_rect.size.x) * cell_size.x
 
 func _process(delta: float) -> void:
-	var speed = 130.0 * delta
-	kitty.position.x += speed
-	camera.position.x += speed
-	print(camera.position.x - tilemap.position.x)
-	print(chunk_width)
-	if camera.position.x - tilemap.position.x > chunk_width * 0.5:
-		tilemap.position.x += chunk_width * 0.5
+	if gamerunning:
+		var speed = 130.0 * delta
+		kitty.position.x += speed
+		camera.position.x += speed
+		
+		if info:
+			if info.position.x > 280:
+				info.queue_free()
+				info = null  
+			else:
+				info.position.x += 0.5 * speed
+
+
+		if camera.position.x - tilemap.position.x > chunk_width * 0.5:
+			tilemap.position.x += chunk_width * 0.5
+	else:
+		if Input.is_action_just_pressed("jump"):
+			gamerunning = true
--- scripts/kitty.gd
+++ scripts/kitty.gd
@@ -2,27 +2,35 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = 250.0
-var gravity_down = true
+var gravity_down: bool = true
+@export var lev : bool
+@onready var light = %Light
+@onready var char = $CollisionShape2D
+var gamerunning : bool
 
 func _physics_process(delta: float) -> void:
+	var gravity = get_gravity().y
+	var up_direction = Vector2.UP if gravity_down else Vector2.DOWN
+	set_up_direction(up_direction)
 	if not is_on_floor():
-		if gravity_down:
-			velocity += get_gravity() * delta
+			velocity.y += gravity * delta if gravity_down else -gravity * delta
+	if gamerunning:
+		# Animations
+		if is_on_floor():
+			$AnimatedSprite2D.play("running")
 		else:
-			velocity -= get_gravity() * delta
-	
-	if is_on_floor():
-		$AnimatedSprite2D.play("running")
+			$AnimatedSprite2D.play("idle_side")
+
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = -JUMP_VELOCITY if gravity_down else JUMP_VELOCITY
+
+		if Input.is_action_just_pressed("gravity"):
+			gravity_down = !gravity_down
+			velocity.y = -JUMP_VELOCITY if gravity_down else JUMP_VELOCITY
+			$AnimatedSprite2D.flip_v = not gravity_down
 	else:
-		$AnimatedSprite2D.play("idle_side")
+		if Input.is_action_just_pressed("jump"):
+			gamerunning = true
+		$AnimatedSprite2D.play("idle_front")
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		if gravity_down:
-			velocity.y = -JUMP_VELOCITY
-		else:
-			velocity.y = JUMP_VELOCITY
-
-	if Input.is_action_just_pressed("gravity") and is_on_floor():
-		gravity_down = !gravity_down
-		
 	move_and_slide()
